794a69bedecd1a57ff286f70ecbb3e9986a952ab
Veronika Romashkina <vrom911@gmail.com>
2020-10-10 14:14:04 +0100

    [#4] Add lens operators
    Resolves #4

--- test/Test/Prolens.hs
+++ test/Test/Prolens.hs
@@ -2,15 +2,22 @@ module Test.Prolens
     ( unitSpecs
     ) where
 
+import Data.Function ((&))
 import Test.Hspec (Spec, describe, it, shouldBe)
 
-import Prolens (set, view)
+import Prolens (set, view, (.~), (^.))
 import Test.Data (me, nameL)
 
 
 unitSpecs :: Spec
 unitSpecs = describe "Prolens unit tests" $ do
-    describe "getter" $
-        it "should get name" $ view nameL me `shouldBe` "Veronika"
-    describe "setter" $
-        it "should get name" $ view nameL (set nameL "Dmitrii" me) `shouldBe` "Dmitrii"
+    describe "getter" $ do
+        it "should get name" $
+            view nameL me `shouldBe` "Veronika"
+        it "should get name with ^." $
+            (me ^. nameL) `shouldBe` "Veronika"
+    describe "setter" $ do
+        it "should set name with .~" $
+            view nameL (set nameL "Dmitrii" me) `shouldBe` "Dmitrii"
+        it "should set name" $
+            (me & nameL .~ "Dmitrii") ^. nameL `shouldBe` "Dmitrii"
